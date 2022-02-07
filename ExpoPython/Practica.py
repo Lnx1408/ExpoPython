74f4359429c03388bacf4789b6226e32efd8e6c9
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2022-02-06 19:00:46 -0500

    Version 1.1 - Primer Commit

---
+++ ExpoPython/Practica.py
@@ -0,0 +1,12 @@
+from turtle import *
+#Version original covid-19
+
+color ('green')
+bgcolor ('black')
+speed(30)
+hideturtle()
+b = 0
+while b < 250:
+    right(b)
+    forward(b * 5)
+    b = b+1
